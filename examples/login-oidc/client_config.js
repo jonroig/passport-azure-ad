bde88a59f465c2a1ff7af4805c85ae9d637523af
Jon Roig <jon@jonroig.com>
2015-07-23 02:42:43 -0700

    stuff

--- examples/login-oidc/client_config.js
+++ examples/login-oidc/client_config.js
@@ -1,10 +1,10 @@
   // Don't commit this file to your public repos. This config is for first-run
   //
  exports.creds = {
- 	returnURL: 'http://localhost:3000/auth/openid/return',
+ 	returnURL: 'http://officecommander.cloudapp.net/auth/openid/return',
  	identityMetadata: 'https://login.microsoftonline.com/common/.well-known/openid-configuration', // For using Microsoft you should never need to change this.
- 	realm: 'http://localhost:3000',
- 	issuer: 'https://sts.windows.net/cff56d8f-f602-4afd-94e4-c95b76f1c81e/',
- 	clientID: 'c9655d1d-f356-46a7-afe1-431c0d6eeb37',
- 	clientSecret: 'jRXwIofdH7Hju2V1E1ab5v5uLVf68mhVgEeIRaBMXf0='
+ 	realm: 'http://officecommander.cloudapp.net',
+ 	issuer: 'https://sts.windows.net/80ac7e51-98f0-4b01-847a-7e45e21df153/',
+ 	clientID: '8e302bed-98ab-4d7d-997a-08f197823789',
+ 	clientSecret: '0bCcmiou3IU6Ezp2Kuv+K/vEJhSA82Vgu9+L4B39dEA='
  };
